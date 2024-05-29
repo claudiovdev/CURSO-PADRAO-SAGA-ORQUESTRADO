01869aa7bd806bbc3ac65c8655e66e3834dd8a9b
claudiovdev <c.viniciusmagalhaes@gmail.com>
2024-05-28 21:16:52 -0300

    mepeando os topicos

---
+++ orchestrator-service/src/main/java/br/com/microservices/orchestrated/orchestratorservice/ETopics.java
@@ -0,0 +1,23 @@
+package br.com.microservices.orchestrated.orchestratorservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum ETopics {
+
+    START_SAGA("start-saga"),
+    BASE_ORCHESTRATOR("orchestrator"),
+    FINISH_SUCCESS("finish-success"),
+    FINISH_FAIL("finish-fail"),
+    PRODUCT_VALIDATION_SUCCESS("product-validation-success"),
+    PRODUCT_VALIDATION_FAILT("product-validation-fail"),
+    PAYMENT_SUCCESS("payment-success"),
+    PAYMENT_SUCCESS_FAILT("payment-fail"),
+    INVENTORY_SUCCESS("inventory-success"),
+    INVENTORY_SUCCESS_FAILT("inventory-fail"),
+    NOTIFY_ENDING("notify-ending");
+
+    private String topic;
+}
